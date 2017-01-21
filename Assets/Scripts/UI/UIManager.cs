95222101fd91a50b8c162572949091a6b68acb06
Elkantor <vic03@live.fr>
2017-01-21 16:10:06 +0100

    testUI

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    public bool paused;
+    private GameObject textPauseGame;
+    private GameObject pauseRestartInput;
+
+    // Use this for initialization
+    void Start () {
+        paused = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void Awake() {
+        textPauseGame = GameObject.Find("PauseTextMessageOnScreen");
+        pauseRestartInput = GameObject.Find("PauseRestartInput");
+        textPauseGame.GetComponent<Text>().enabled = false;
+    }
+
+    //Reloads the Level
+    public void Reload() {
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+
+    public void Pause() {
+        paused = !paused;
+        
+        if (paused) {
+            Time.timeScale = 0;
+            textPauseGame.GetComponent<Text>().enabled = true;
+            pauseRestartInput.GetComponent<Image>().sprite = Resources.Load<Sprite>("Icon_Play");
+        }
+        else if (!paused) {
+            Time.timeScale = 1;
+            textPauseGame.GetComponent<Text>().enabled = false;
+            pauseRestartInput.GetComponent<Image>().sprite = Resources.Load<Sprite>("Icon_Pause");
+        }
+
+    }
+}
