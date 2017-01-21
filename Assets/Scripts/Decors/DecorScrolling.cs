6823a4b83cf514d22a55585bec51c40cb13b577e
Marc Portet <marc@webshaped.fr>
2017-01-21 21:07:42 +0100

    Beginning of the merge for prototyping the game

---
+++ Assets/Scripts/Decors/DecorScrolling.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DecorScrolling : MonoBehaviour
+{
+
+    public float speed = -3f;
+
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0f, 0f);
+    }
+}
