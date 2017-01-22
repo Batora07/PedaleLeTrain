e5e79b5917d1b9b93950afd284e050fd0a36056f
Elkantor <vic03@live.fr>
2017-01-22 03:27:09 +0100

    mobile

--- Assets/Scripts/Surfer.cs
+++ Assets/Scripts/Surfer.cs
@@ -11,8 +11,11 @@ public class Surfer : MonoBehaviour {
 	private bool jumping = false;
 	private bool canjump = false;
 	public Train train;
+    private Button pauseButton;
+    private bool paused;
+    private UIManager uimanager;
 
-	public int currentSpeed = 0;
+    public int currentSpeed = 0;
 	public float[] speeds = new float[6] { 4f, 2f, 3f, 4f, 5f, 6f };
 	public float[] maxFallSpeeds = new float[6];
 	public float[] gravities = new float[6] { 9.81f, 9.81f, 9.81f, 9.81f, 9.81f, 9.81f };
@@ -33,8 +36,10 @@ public class Surfer : MonoBehaviour {
 			turning = false;
 			canjump = false;
 		}
-		if (jumping) {
-			if (Input.GetButton ("Jump")) {
+        /* Inputs mobile*/
+        MobileJump();
+        if (jumping) {
+			if (Input.GetButton ("Jump") || Input.touchCount > 0) {
 				transform.RotateAround (GetComponent<Collider2D> ().bounds.center, Vector3.back, -turnRatesJump[currentSpeed] * Time.deltaTime);
 			} else {
 				float angle = Quaternion.Angle (transform.rotation, Quaternion.identity);
@@ -84,4 +89,19 @@ public class Surfer : MonoBehaviour {
 		if (grounded > 0)
 			grounded--;
 	}
+
+    public void MobileJump()
+    {
+        if (Input.touchCount > 0 && grounded > 0)
+        {
+            if (EventSystem.current.lastSelectedGameObject == GameObject.Find("PauseRestartInput") ||
+                EventSystem.current.currentSelectedGameObject == GameObject.Find("PauseRestartInput"))
+            {
+                return;
+            }
+            fallSpeed = speeds[currentSpeed];
+            grounded = 0;
+            jumping = true;
+        }
+    }
 }
